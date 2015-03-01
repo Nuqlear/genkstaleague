2b7ce596165c4fc606d0fbda80dadb5f68d5969d
Nuqlear <undergroundcateatskitekat@gmail.com>
2015-03-01 12:35:33 +0500

    add favicon and sitemap

--- gleague/frontend/__init__.py
+++ gleague/frontend/__init__.py
@@ -1,9 +1,11 @@
-from flask import jsonify, g, Response, current_app, url_for, redirect
+from flask import jsonify, g, Response, current_app, url_for, redirect, render_template, make_response, send_from_directory
 from functools import wraps
 from flask_openid import OpenID
+from datetime import datetime, timedelta
 
 from .. import core
 from .. import admin
+from ..models import Player, Match
 
 oid = OpenID()
 
@@ -26,9 +28,43 @@ def create_app(settings_override=None):
     from .auth import auth_bp
     app.register_blueprint(auth_bp)
 
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(app.static_folder, 'favicon.ico')
+
     @app.route('/')
     def redirect_to_matches():
         return redirect(url_for('players.players'))
+
+    @app.route('/sitemap.xml', methods=['GET'])
+    def sitemap():
+          """Generate sitemap.xml. Makes a list of urls and date modified."""
+          pages=[]
+          ten_days_ago=datetime.now() - timedelta(days=10)
+          # static pages
+          for rule in app.url_map.iter_rules():
+            print(dir(rule))
+            if "GET" in rule.methods and len(rule.arguments)==0:
+                pages.append(
+                    [rule.rule,ten_days_ago]
+                )
+          
+          players = Player.query.order_by(Player.steam_id).all()
+          for player in players:
+              url=url_for('players.player_overview', steam_id=player.steam_id)
+              pages.append([url,ten_days_ago])
+
+          matches = Match.query.order_by(Match.id).all()
+          for match in matches:
+              url=url_for('matches.match', match_id=match.id)
+              modified_time=datetime.fromtimestamp(match.start_time).date().isoformat()
+              pages.append([url,modified_time]) 
+
+          sitemap_xml = render_template('sitemap_template.xml', pages=pages)
+          response= make_response(sitemap_xml)
+          response.headers["Content-Type"] = "application/xml"    
+        
+          return response
 
     @app.context_processor
     def inject_globals():
