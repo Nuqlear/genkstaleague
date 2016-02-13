9c90e0bae60c3c6a50becfb3895f8a7fbe2b5439
Nuqlear <undergroundcateatskitekat@gmail.com>
2016-02-14 04:20:20 +0500

    urlopen readall() problem fixed

--- gleague/utils/steam_api.py
+++ gleague/utils/steam_api.py
@@ -11,7 +11,7 @@ def get_steam_user_info(steam_id, steam_api_key):
     }
     url = 'http://api.steampowered.com/ISteamUser/' \
           'GetPlayerSummaries/v0001/?%s' % url_encode(options)
-    rv = json.loads(urlopen(url).readall().decode())
+    rv = json.loads(urlopen(url).read().decode())
     return rv['response']['players']['player'][0] or {}
 
 
@@ -21,7 +21,7 @@ def get_dota2_heroes(steam_api_key):
     }
     url = 'http://api.steampowered.com/IEconDOTA2_570/' \
           'GetHeroes/V1/?%s' % url_encode(options)
-    res = json.loads(urlopen(url).readall().decode())
+    res = json.loads(urlopen(url).read().decode('utf-8'))
     d = dict()
     for i in res['result']['heroes']:
         d[i['id']] = i['name']
