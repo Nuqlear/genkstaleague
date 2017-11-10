faacacfd091d1e1588d786ff8fdc28a04c5caa2e
Nuqlear <undergroundcateatskitekat@gmail.com>
2017-11-11 02:53:38 +0300

    team_builder returned

--- gleague/frontend/matches.py
+++ gleague/frontend/matches.py
@@ -1,3 +1,6 @@
+import random
+from collections import namedtuple
+
 from flask import Blueprint
 from flask import abort
 from flask import current_app
@@ -5,7 +8,7 @@ from flask import render_template
 from flask import request
 from sqlalchemy import desc
 
-from gleague.models import Match
+from gleague.models import Match, Season, SeasonStats, Player
 
 
 matches_bp = Blueprint('matches', __name__)
@@ -29,3 +32,68 @@ def matches_preview():
         page, current_app.config['HISTORY_MATCHES_PER_PAGE'], True
     )
     return render_template('matches.html', matches=m)
+
+
+@matches_bp.route('/team_builder', methods=['GET', 'POST'])
+def team_builder():
+    PlayerTuple = namedtuple('PlayerTuple', ['nickname', 'pts'], verbose=True)
+    cs_id = Season.current().id
+    season_stats = (
+        sorted(
+            SeasonStats.query.filter(SeasonStats.season_id==cs_id).all(),
+            key=lambda ss: ss.player.nickname.lower()
+        )
+    )
+    context = {
+        'season_stats': season_stats
+    }
+    if request.method == 'POST':
+        players = []
+        for i in range(1, 11):
+            player_id = request.form.get('player-%i' % i)
+            if player_id == 'None':
+                players.append(
+                    PlayerTuple(
+                        'NOT REGISTERED PLAYER', SeasonStats.pts.default.arg
+                    )
+                )
+            else:
+                p = Player.query.get(player_id)
+                players.append(PlayerTuple(p.nickname, p.season_stats[0].pts))
+        context['teams'] = sort_by_pts(players)
+    return render_template('team_builder.html', **context)
+
+
+def sort_by_pts(players, t=50):
+
+    def total_pts(players):
+        return sum((players[i].pts for i in range(len(players))))
+
+    def pts_diff(radiant, dire):
+        return abs(total_pts(radiant) - total_pts(dire))
+
+    def shuffle(players):
+        teams = [[], []]
+        radiant = teams[0]
+        dire = teams[1]
+        for i in range(0, len(players), 2):
+            if random.randrange(0, 2):
+                radiant.append(players[i])
+                dire.append(players[i + 1])
+            else:
+                radiant.append(players[i + 1])
+                dire.append(players[i])
+        return teams
+
+    sorted_players = sorted(players, key=lambda p: p.pts)
+    best_attempt = shuffle(sorted_players)
+    best_diff = pts_diff(*best_attempt)
+
+    for __ in range(t):
+        attempt = shuffle(sorted_players)
+        diff = pts_diff(*attempt)
+        if diff < best_diff:
+            best_diff = diff
+            best_attempt = attempt
+
+    return best_attempt
