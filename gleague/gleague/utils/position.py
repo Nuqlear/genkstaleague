118eb47b6e3a0445d113cca72fddee232614e54b
jemdo <ijemdo@gmail.com>
2023-04-20 15:16:28 +0500

    switch back to top/mid/bot scheme, remove kmeans use zoning for positioning instead

--- gleague/gleague/utils/position.py
+++ gleague/gleague/utils/position.py
@@ -3,8 +3,8 @@ from collections import OrderedDict
 from collections import Counter
 from typing import Optional, List
 
-from sklearn.cluster import KMeans
-import numpy as np
+
+MID_THRESHOLD = 2000
 
 
 class Position(enum.Enum):
@@ -12,37 +12,23 @@ class Position(enum.Enum):
     bottom = "bottom"
     middle = "middle"
     roam = "roam"
-    safelane = "safelane"
-    offlane = "offlane"
 
 
-positions_radiant = OrderedDict(
-    {
-        (4500, 8000): Position.offlane,
-        (7500, 5500): Position.safelane,
-        (7200, 7200): Position.middle,
-    }
-)
-
-positions_dire = OrderedDict(
-    {
-        (7000, 10000): Position.safelane,
-        (10500, 8000): Position.offlane,
-        (7200, 7200): Position.middle,
-    }
-)
+def point_position(point: List[int]):
+    if abs(pair[0] - pair[1]) < MID_THRESHOLD:
+        return Position.middle
+    if pair[0] - pair[1] < 0:
+        return Position.top
+    return Position.bottom
 
 
-def detect_position(points: List[dict], is_radiant: bool) -> Optional[Position]:
+def detect_position(points: List[List[int]]) -> Optional[Position]:
     if not points:
         return None
-    positions = positions_radiant if is_radiant else positions_dire
-    init = np.array(list(positions.keys()))
-    kmeans = KMeans(n_clusters=3, init=init)
-    result = kmeans.fit(points).labels_
+    result = [point_position(point) for point in points]
     most_common, num_most_common = Counter(result).most_common(1)[0]
     if (len(points) / num_most_common) > 0.6:
-        position = list(positions.items())[most_common][-1]
+        position = most_common
     else:
         position = Position.roam
     return position
